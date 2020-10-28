13b1ec70a542102e477ee25350d4e97fef6eeffb
Nicholas <berubenic@gmail.com>
2020-10-28 19:04:10 -0400

    fresh idea

--- lib/chess/move.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Move checker class, builds tree with possible moves
-class Move
-  def initialize(coord)
-    @coord = coord
-    @possible_moves = nil
-  end
-end
